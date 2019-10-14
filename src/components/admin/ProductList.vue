dbdfaa4fd1b8338e1ed65c373048bab68cee5af6
sandrine <sandrinezocca@gmail.com>
2019-10-14 18:00:13 +0200

    ajout css tableau admin

--- src/components/admin/ProductList.vue
+++ src/components/admin/ProductList.vue
@@ -1,28 +1,43 @@
 <template>
   <div class="flex justify-center">
-    <table class="table-auto border-separate">
-      <thead>
-        <tr>
-          <th class="px-4 py-2">id</th>
-          <th class="px-4 py-2">Title</th>
-          <th class="px-4 py-2">Author</th>
-          <th class="px-4 py-2">Publisher</th>
-          <th class="px-4 py-2">Year</th>
-          <th class="px-4 py-2">Image</th>
-          <th class="px-4 py-2"></th>
-          <th class="px-4 py-2"></th>
+    <table class="table-auto">
+      <thead class="bg-gray-200 uppercase text-grey-400">
+        <tr class="border-t border-gray-300 text-gray-600">
+          <th class="px-10 py-2 ">id</th>
+          <th class="px-10 py-2">Title</th>
+          <th class="px-10 py-2">Author</th>
+          <th class="px-10 py-2">Publisher</th>
+          <th class="px-10 py-2">Year</th>
+          <th class="px-10 py-2">Image</th>
+          <th class="px-10 py-2"></th>
         </tr>
       </thead>
       <tbody>
-        <tr class="bg-transparent hover:bg-gray-100">
-          <th class="border px-4 py-2">1</th>
-          <td class="border px-4 py-2">aze</td>
-          <td class="border px-4 py-2">qsd</td>
-          <td class="border px-4 py-2">xwc</td>
-          <td class="border px-4 py-2">2019</td>
-          <td class="border px-4 py-2">image</td>
-          <td class="border border-yellow-500 px-4 py-2">edit</td>
-          <td class="border border-red-500 px-4 py-2">delete</td>
+        <tr class="hover:bg-blue-100 border-t border-gray-300">
+          <th class="p-4">1</th>
+          <td class="p-4">aze</td>
+          <td class="p-4">qsd</td>
+          <td class="p-4">xwc</td>
+          <td class="p-4">2019</td>
+          <td class="p-4">image</td>
+          <td class="p-4 text-blue-500">
+            <button class="px-2 hover:bg-white">Edit</button>
+            <span>|</span>
+            <button class="px-2 hover:bg-white">Delete</button>
+          </td>
+        </tr>
+        <tr class="hover:bg-blue-100 border-t border-gray-300">
+          <th class="p-4">1</th>
+          <td class="p-4">aze</td>
+          <td class="p-4">qsd</td>
+          <td class="p-4">xwc</td>
+          <td class="p-4">2019</td>
+          <td class="p-4">image</td>
+          <td class="p-4 text-blue-500">
+            <button class="px-2 hover:bg-white">Edit</button>
+            <span>|</span>
+            <button class="px-2 hover:bg-white">Delete</button>
+          </td>
         </tr>
       </tbody>
     </table>
