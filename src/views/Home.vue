f53b5705738a2ad7961eaea3c66c34242209c011
sandrine <sandrinezocca@gmail.com>
2019-10-18 17:41:45 +0200

    Afficher les produits sur la page d'accueil

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="text-5xl ">
-    <h1>Book Shop</h1>
+  <div>
+    <h1 class="text-5xl text-center pb-4 ">Book Shop</h1>
+    <Products/>
   </div>
 </template>
 
 <script>
+import Products from '@/views/Products';
 
 export default {
   name: "home",
+  components: {
+    Products,
+  },
 };
 </script>
---
+++ src/views/Products.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-wrap" v-if="products">
+    <div class="relative flex-col justify-between p-2 m-2 w-64 shadow" v-for="product in products" :key="product.id">
+      <div class="p-4">{{ product.title }}</div>
+      <div class="flex justify-center p-2">
+        <img class="w-auto inline-block shadow-2xl mb-12" alt="book image" :src="product.image" />
+      </div>
+      <div class="absolute inset-x-0 bottom-0 flex flex-row justify-around">
+        <div class="text-3xl">{{ product.price }}<span class="text-base font-semibold"> €</span></div>
+        <button class="text-blue-500 font-extrabold underline">Add to cart</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$store.dispatch("getProducts");
+  },
+  computed: {
+    products() {
+      return this.$store.state.products;
+    }
+  }
+};
+</script>
